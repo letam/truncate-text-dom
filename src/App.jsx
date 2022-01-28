f79b258942a24b600fe83cbee30bff5652973bf3
Tam Le <tamsgit@gmail.com>
2022-01-28 17:52:56 -0500

    Update dummy text to test truncation component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import './App.css';
 
 import TruncateText from './TruncateText';
 
+const DUMMY_TEXT =
+  'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
 function App() {
   return (
     <>
       <div style={{ margin: '16px' }}>
         <div style={{ border: '1px dotted gray', width: '360px' }}>
-          <TruncateText>Derpity derp derp</TruncateText>
+          <TruncateText>{DUMMY_TEXT}</TruncateText>
         </div>
       </div>
       <div style={{ marginTop: '16px' }} className="App">
