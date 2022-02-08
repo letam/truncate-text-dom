2824315ddef534cad1cea584d62ce52fed887ac9
Tam Le <tamsgit@gmail.com>
2022-02-08 08:37:27 -0500

    Update names of base Storybook stories

--- src/components/TruncateText.stories.jsx
+++ src/components/TruncateText.stories.jsx
@@ -22,14 +22,14 @@ const Template = (args) => (
 const DUMMY_TEXT =
   'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
-// 1 Line
-export const Case1 = Template.bind({});
-Case1.args = {
+// 1 Line - no truncation
+export const NoTruncateOnSingleLine = Template.bind({});
+NoTruncateOnSingleLine.args = {
   children: 'Lorem ipsum',
 };
 
-// Lots of lines
-export const Case2 = Template.bind({});
-Case2.args = {
+// Lots of lines - should truncate
+export const TruncateOnOverflow = Template.bind({});
+TruncateOnOverflow.args = {
   children: DUMMY_TEXT,
 };
