483312c752aab02c563efbf1117dc16703dd8fa2
Tam Le <tamsgit@gmail.com>
2022-02-11 14:08:28 -0500

    Set up Storybook to use Vite

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,12 @@
 module.exports = {
+  core: {
+    builder: 'storybook-builder-vite',
+  },
   framework: '@storybook/react',
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  async viteFinal(config, { configType }) {
+    // customize the Vite config here
+    return config;
+  },
 };
